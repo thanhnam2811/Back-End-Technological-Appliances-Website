a1561e344957086156744607802e48c2c2965d69
Thành Nam <thanhnam.thai01@gmail.com>
2022-04-17 16:25:57 +0700

    add utils file

---
+++ src/main/java/com/hcmute/backendtechnologicalapplianceswebsite/utils/fileUtils/dowload/FileDowloadUtil.java
@@ -0,0 +1,27 @@
+package com.hcmute.backendtechnologicalapplianceswebsite.utils.fileUtils.dowload;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileDowloadUtil {
+    private Path foundFile;
+
+    public Resource getFileAsResource(String fileCode) throws IOException {
+        Path uploadDirectory = Paths.get("src/main/resources/static/images");
+        Files.list(uploadDirectory).forEach(file -> {
+            if (file.getFileName().toString().startsWith(fileCode)) {
+                foundFile = file;
+            }
+        });
+        if (foundFile != null) {
+            return new UrlResource(foundFile.toUri());
+        }
+        return null;
+    }
+
+}
