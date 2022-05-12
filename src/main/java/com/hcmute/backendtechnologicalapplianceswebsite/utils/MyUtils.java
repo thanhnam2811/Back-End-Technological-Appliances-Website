a977db4807e6e04301fac1f6dd60d0abf6a291d5
Thành Nam <thanhnam.thai01@gmail.com>
2022-05-12 21:37:02 +0700

    move MyUtils from fileUtils to utils

--- src/main/java/com/hcmute/backendtechnologicalapplianceswebsite/utils/MyUtils.java
+++ src/main/java/com/hcmute/backendtechnologicalapplianceswebsite/utils/MyUtils.java
@@ -1,12 +1,8 @@
-package com.hcmute.backendtechnologicalapplianceswebsite.utils.fileUtils;
-
-import com.hcmute.backendtechnologicalapplianceswebsite.model.Order;
-
-import java.util.List;
+package com.hcmute.backendtechnologicalapplianceswebsite.utils;
 
 public class MyUtils {
     public static String generateID(String PREFIX, int length, String lastIdStr) {
-        Long lastId = Long.parseLong(lastIdStr.substring(PREFIX.length()));
+        long lastId = Long.parseLong(lastIdStr.substring(PREFIX.length()));
 
         while (lastId >= Math.pow(10, length))
             length = length + 1;
