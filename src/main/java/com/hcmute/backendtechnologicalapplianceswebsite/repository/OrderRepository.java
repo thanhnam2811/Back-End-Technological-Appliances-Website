8eb1d1306fe6cccd4119c154aaddd14d6c7784c0
Thành Nam <thanhnam.thai01@gmail.com>
2022-05-12 21:16:59 +0700

    update generateId for OrderRepository

--- src/main/java/com/hcmute/backendtechnologicalapplianceswebsite/repository/OrderRepository.java
+++ src/main/java/com/hcmute/backendtechnologicalapplianceswebsite/repository/OrderRepository.java
@@ -12,18 +12,19 @@ import java.util.Optional;
 public interface OrderRepository extends JpaRepository<Order, String> {
     Order findByOrderId(String id);
     default String generateOrderId() {
+        String PREFIX = "O";
 
         List<Order> orders = findAll();
 
         orders.sort((c1, c2) -> {
-            Long id1 = Long.parseLong(c1.getOrderId().substring(1));
-            Long id2 = Long.parseLong(c2.getOrderId().substring(1));
+            Long id1 = Long.parseLong(c1.getOrderId().substring(PREFIX.length()));
+            Long id2 = Long.parseLong(c2.getOrderId().substring(PREFIX.length()));
             return id2.compareTo(id1);
         });
 
         String lastOrderId = orders.get(0).getOrderId();
 
-        return "OD" + String.format("%05d", Long.parseLong(lastOrderId.substring(2)) + 1);
+        return PREFIX + String.format("%05d", Long.parseLong(lastOrderId.substring(2)) + 1);
     }
 
     Iterable<Order> findAllByUser(User user);
